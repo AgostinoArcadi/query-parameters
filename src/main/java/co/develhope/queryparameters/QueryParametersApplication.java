e9d12c8db6657d40ccfa16eddb7ebbb96674cddb
Agostino <agostinoarcadi@gmail.com>
2024-02-16 15:27:03 +0100

    primo

---
+++ src/main/java/co/develhope/queryparameters/QueryParametersApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.queryparameters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryParametersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryParametersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/queryparameters/QueryParametersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.queryparameters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryParametersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
